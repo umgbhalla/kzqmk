4fc7b17633c5ff8b09e6665ff963c77ee1bc6090
umgbhalla <umg.bhalla88@gmail.com>
2023-09-16 19:42:46 +0530

    added updated qmk config

--- keyboards/sporewoh/keezyboost40/config.h
+++ keyboards/sporewoh/keezyboost40/config.h
@@ -17,28 +17,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+// #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xBEAF
-#define PRODUCT_ID      0x0003
-#define DEVICE_VER      0x0000
-#define MANUFACTURER    sporewoh
-#define PRODUCT         keezyboost40
+// #define VENDOR_ID       0xBEAF
+// #define PRODUCT_ID      0x0003
+// #define DEVICE_VER      0x0000
+// #define MANUFACTURER    sporewoh
+// #define PRODUCT         keezyboost40
 
 /* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 10
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 12
 
 /* pin-out for PCB */
-#define MATRIX_ROW_PINS { GP5, GP6, GP7, GP8 }
-#define MATRIX_COL_PINS { GP27, GP26, GP22, GP21, GP20, GP4, GP3, GP2, GP1, GP0 }
+#define MATRIX_ROW_PINS { GP5, GP6, GP7, GP8,GP9}
+#define MATRIX_COL_PINS { GP27, GP26, GP22, GP21, GP20, GP4, GP3, GP2, GP1, GP0, GP23, GP24 }
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 
-#define RGB_DI_PIN A1
+// #define RGB_DI_PIN A1
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -51,7 +51,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPI_MOSI_PIN GP19
 // We don't actually use MISO, so set it to a pin we don't use.
 // Otherwise it likely get's tripped up as we use GP16 for D/C
-#define SPI_MISO_PIN GP10 
+#define SPI_MISO_PIN GP10
 
 #define QUANTUM_PAINTER_SUPPORTS_256_PALETTE TRUE
 #define QUANTUM_PAINTER_LOAD_FONTS_TO_RAM TRUE
